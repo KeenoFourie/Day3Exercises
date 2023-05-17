4e64648d3a38a52d59ccda56416e2f11f4ffc6c7
KeenoFourie <keenofourie614@gmail.com>
2023-05-17 09:16:06 +0200

    first commit

---
+++ script/code.js
@@ -0,0 +1,23 @@
+// Question 1
+// let name = "Keeno";
+// console.log(typeof name);
+
+// Question 2
+let name = 7;
+console.log(typeof name);
+
+// Question 3
+let secondName = "Tom";
+console.log(typeof secondName);
+
+let num = 8;
+console.log(typeof num);
+
+let question = true;
+console.log(typeof question);
+
+let surname;
+console.log(typeof surname);
+
+let color = null;
+console.log(typeof color);
